f9538348a51795db3a8c5956ed401f1f8e795220
blatube <admin@blatube.com>
2020-11-27 02:37:10 +0300

    TASK-2: добавил группировку входного массива билетов по цене

--- task2.php
+++ task2.php
@@ -53,9 +53,21 @@ $orders = [
 ];
 
 function createReport(array $orders):array {
-    $lines = array();
+    $lines = [];
 
-    /** @todo ПИШИТЕ ВАШ КОД ЗДЕСЬ*/
+    foreach ($orders as $k => $order) {
+        $countTicketsChunks = [];
+
+        foreach ($order['tickets'] as $tickets) {
+            $countTicketsChunks[$tickets['price']] = isset($countTicketsChunks[$tickets['price']])
+                ? ++$countTicketsChunks[$tickets['price']]
+                : 1;
+        }
+
+        foreach ($countTicketsChunks as $key => $item) {
+            $lines[] = ['orderId' => $order['id'], 'count' => $item, 'sum' => $key * $item ];
+        }
+    }
 
     return $lines;
 }
